faadc1d294f1d808237f0903592a0201824cda41
auglxw <augustineleexuanwei@gmail.com>
2022-08-01 17:07:16 +0800

    Edited confirmation page to show timing dynamically

--- client/src/pages/Confirmation.jsx
+++ client/src/pages/Confirmation.jsx
@@ -2,14 +2,17 @@ import "../App.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import React, { useState } from "react";
 import Header from "../components/Header";
+import { useSelector } from "react-redux";
 
 function BookingConfirmed() {
+    const patientDetails = useSelector(state => state.details)
+
     return <div className="confirmation">
         <Header />
         <div className="bookingCard">
             <img src={require("../img/confirmation_tick.png")} className="confirmationTick"></img>
             <p className="bookingHeading">Booking Success!</p>
-            <p>Your doctor's appointment for 2.00PM has been confirmed.</p>
+            <p>Your doctor's appointment for {patientDetails.timing} has been confirmed.</p>
             <p>You may check your queue status from the home page.</p>
         </div>
     </div>
