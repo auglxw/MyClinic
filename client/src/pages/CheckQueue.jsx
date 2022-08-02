04c16144fb9321fe4f2b402a1c49f20dad7ba3ea
auglxw <augustineleexuanwei@gmail.com>
2022-08-03 00:00:14 +0800

    Added get request for checkqueue page

--- client/src/pages/CheckQueue.jsx
+++ client/src/pages/CheckQueue.jsx
@@ -2,15 +2,21 @@ import "../App.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import React, { useState } from "react";
 import Header from "../components/Header";
+import Axios from "axios";
 
 function CheckQueue() {
+    async function getStatus(nric, contact) {
+        let status = await Axios.get("http://localhost:3001/checkqueue", {"nric" : nric, "contact" : contact});
+        return status;
+    }
+
     return <div className="checkQueue">
         <Header />
-        <div className="bookingCard">
+        <div className="bookingCard"> 
             <p className="bookingHeading">Check Queue Status</p>
-            <form className="registrationForm" action={true ? "/timing" : "/details"} method="post" >
-                    <input type="text" name="nric" placeholder="NRIC"  />
-                    <input type="text" name="contactnumber" placeholder="Contact Number" />
+            <form className="registrationForm" action={true ? "/queuestatus" : "/checkqueue"} >
+                    <input type="text" placeholder="NRIC"  />
+                    <input type="text" placeholder="Contact Number" />
                     <div className="bookingNext">
                         <button className="pillButton" >Next</button>
                     </div>
