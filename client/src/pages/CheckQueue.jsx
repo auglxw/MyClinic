398f6bde5d366586a385586a885f8f269f94fe91
auglxw <augustineleexuanwei@gmail.com>
2022-08-03 12:31:35 +0800

    Updated getqueuestatus page state management

--- client/src/pages/CheckQueue.jsx
+++ client/src/pages/CheckQueue.jsx
@@ -5,20 +5,30 @@ import Header from "../components/Header";
 import Axios from "axios";
 
 function CheckQueue() {
+    const [nric, updateNRIC] = useState('');
+    const [contact, updateContact] = useState('');
+    const [formComplete, updateFormComplete] = useState(false);
+    const [queuestatus, updateStatus] = useState(0);
+
     async function getStatus(nric, contact) {
+        if (nric.length < 9 && contact.length < 8) {
+            return updateFormComplete(false);
+        }
+
+        updateFormComplete(true);
         let status = await Axios.get("http://localhost:3001/checkqueue", {"nric" : nric, "contact" : contact});
-        return status;
+        return updateStatus(status);
     }
 
     return <div className="checkQueue">
         <Header />
         <div className="bookingCard"> 
             <p className="bookingHeading">Check Queue Status</p>
-            <form className="registrationForm" action={true ? "/queuestatus" : "/checkqueue"} >
-                    <input type="text" placeholder="NRIC"  />
-                    <input type="text" placeholder="Contact Number" />
+            <form className="registrationForm" action={formComplete ? "/queuestatus" : "/checkqueue"} >
+                    <input type="text" placeholder="NRIC" onChange={(e) => updateNRIC(e.target.value)} />
+                    <input type="text" placeholder="Contact Number" onChange={(e) => updateContact(e.target.value)} />
                     <div className="bookingNext">
-                        <button className="pillButton" >Next</button>
+                        <button className="pillButton" onClick={() => getStatus(nric, contact)} >Next</button>
                     </div>
                 </form>
         </div>
