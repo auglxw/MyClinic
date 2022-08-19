bc151b35a14345a43ceb4642ad5e9e26cff3c911
auglxw <augustineleexuanwei@gmail.com>
2022-08-19 23:51:56 +0800

    Added authentication system for employees

--- client/src/components/LoginForm.jsx
+++ client/src/components/LoginForm.jsx
@@ -1,21 +1,37 @@
 import "../App.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import * as React from "react";
-import { Form, Button } from "react-bootstrap";
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { Form } from "react-bootstrap";
 
 function Loginform() {
+    let navigate = useNavigate();
+    const [username, updateUsername] = useState("");
+    const [password, updatePassword] = useState("");
+    const [authenticated, updateAuthentication] = useState(false);
+
+    function authenticate() {
+        if (username==process.env.REACT_APP_LOGIN_USERNAME && password==process.env.REACT_APP_LOGIN_PASSWORD) { /* checks if username and password match */
+            updateAuthentication(true);
+        }
+    }
+
+    useEffect(() => {
+        authenticated && navigate("/"); /*navigate to targeted page if autheticated */
+    });
+
     return <div className="loginform">
         <Form>
             <Form.Group className="mb-3">
                 <Form.Label className="loginformlabel">Username</Form.Label>
-                <Form.Control placeholder="Enter Username" />
+                <Form.Control onChange={(e) => {updateUsername(e.target.value)}} placeholder="Enter Username" />
             </Form.Group>
             <Form.Group className="mb-3">
                 <Form.Label className="loginformlabel">Password</Form.Label>
-                <Form.Control type="password" placeholder="Enter Password" />
+                <Form.Control onChange={(e) => {updatePassword(e.target.value)}} type="password" placeholder="Enter Password" />
             </Form.Group>
         </Form>
-        <button className="loginButton">Log In</button>
+        <button onClick={authenticate} className="loginButton" >Log In</button>
     </div>
 }
 
